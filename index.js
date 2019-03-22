ffff1678fc2572d5ad526f4d2ff7b9ed099c12d6
Katherine Wren <kwren@outlook.com>
2019-03-22 18:00:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // write your code below!
 function happyHolidays() {return 'Happy holidays!'}
-function happyHolidaysTo(script) {return 'Happy holidays, ${script}!'}
+function happyHolidaysTo(Happy holidays, script) {return '${Happy holidays}, ${script}!'}
