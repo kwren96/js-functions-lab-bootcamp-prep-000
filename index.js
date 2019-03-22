472e9a0b91835cc6ac31d62d154b3b2717d260c5
Katherine Wren <kwren@outlook.com>
2019-03-22 18:07:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 function happyHolidays() {return 'Happy holidays!'}
 function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
 function happyHolidayTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
-function holidayCountdown(dayname,days) {return `It\'s ${days} until ${dayname}!`)
+function holidayCountdown(dayname,days) {return `It\'s ${days} until ${dayname}!`}
